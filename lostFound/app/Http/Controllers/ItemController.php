a0e92e0eba65635c7c1df3c5e20809009f1e8901
cadis <nouvailbravis@gmail.com>
2025-12-09 17:46:34 +0700

    Fitur Memberi Info Barang Hilang

---
+++ lostFound/app/Http/Controllers/ItemController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use App\Models\Item;
+
+class ItemController extends Controller
+{
+    public function create()
+    {
+        return view('lapor-hilang');
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nama_barang' => 'required',
+            'lokasi'      => 'required',
+            'tanggal'     => 'required|date',
+            'foto_barang' => 'required|image|mimes:jpeg,png,jpg|max:5048',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $pathFoto = null;
+        if ($request->hasFile('foto_barang')) {
+            $pathFoto = $request->file('foto_barang')->store('barang_hilang', 'public');
+        }
+
+        $item = Item::create([
+            'nama_barang' => $request->nama_barang,
+            'foto_barang' => $pathFoto,
+            'lokasi'      => $request->lokasi,
+            'tanggal'     => $request->tanggal,
+            'tipe'        => 'kehilangan',
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Data berhasil masuk database!',
+            'data' => $item
+        ], 201);
+    }
+}
